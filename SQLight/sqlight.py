acd7065e3cce759d07fc5eb27761aba85543c29f
SensenTV <MalestGmbHundKo.KG@gmx.de>
2025-05-11 15:24:00 +0200

    Database Addons

--- SQLight/sqlight.py
+++ SQLight/sqlight.py
@@ -1,18 +1,42 @@
 import sqlite3
 
+# Verbindung zur Datenbank
 connection = sqlite3.connect('farming_station.db')
 cursor = connection.cursor()
 
-cursor.execute(""
-               "CREATE TABLE IF NOT EXISTS beispiel "
-               "( id INTEGER PRIMARY KEY, "
-               "name TEXT)")
+# Tabelle 'beispiel' erstellen
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS beispiel (
+        id INTEGER PRIMARY KEY,
+        name TEXT
+    )
+""")
 
-cursor.execute(""
-               "CREATE TABLE IF NOT EXISTS pH_Value"
-               "(pH_id INTEGER PRIMARY KEY,"
-               "Value FLOAT,"
-               "Timestamp SMALLDATETIME)")
+# Tabelle 'pH_Value' erstellen
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS pH_Value (
+        pH_id INTEGER PRIMARY KEY,
+        Value FLOAT,
+        Timestamp TEXT
+    )
+""")
 
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS EC_Value (
+        EC_id INTEGER PRIMARY KEY,
+        Value FLOAT,
+        Timestamp TEXT
+    )
+""")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Temperature (
+        temp_id INTEGER PRIMARY KEY,
+        Value FLOAT,
+        Timestamp TEXT
+    )
+""")
+
+# Änderungen speichern und Verbindung schließen
 connection.commit()
 connection.close()
