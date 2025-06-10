8a79cd08152b51490ddc5fd5274d3a4f0dc5a8bb
SensenTV <stevenhaupenthal@gmx.de>
2025-06-10 16:47:08 +0200

    Code für DHT11 Temperatur und Luftfeuchtigkeit Sensor

---
+++ Sensors/sensors.py
@@ -0,0 +1,27 @@
+import time
+
+import board
+
+import adafruit_dht
+
+# DHT11 Temperature and Humidity Sensor
+# Initial the dht device, with data pin connected to:
+dhtDevice = adafruit_dht.DHT11(board.D4)
+
+while True:
+    try:
+        # Print the values to the serial port
+        temperature_c = dhtDevice.temperature
+        humidity = dhtDevice.humidity
+        print(f"Temp: {temperature_c:.1f} C    Humidity: {humidity}% ")
+
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
+
+    time.sleep(2.0)
