2792d64396e5c86380863f2ec2e01748c7140523
SensenTV <stevenhaupenthal@gmx.de>
2025-06-21 00:21:04 +0200

    Ultrasonic Sensor test und Ultrasonic Sensor im Haupt Sensor code

--- Sensors/sensors.py
+++ Sensors/sensors.py
@@ -2,19 +2,14 @@ import time
 import board
 import adafruit_dht
 import Adafruit_ADS1x15
-
-# DHT11 Temperature and Humidity Sensor
-# Initial the dht device, with data pin connected to:
-dhtDevice = adafruit_dht.DHT11(board.D4)
-# Water Sensor
-# Initial the Device
-ADC = Adafruit_ADS1x15.ADS1115(busnum= 1)
-ADC_Channel = 3
-Gain = 1
-Min_ADC_Value = 0
-Max_ADC_Value = 32767 
+import adafruit_hcsr04
 
 def dht_sensor():
+
+    # DHT11 Temperature and Humidity Sensor
+    # Initial the dht device, with data pin connected to:
+    dhtDevice = adafruit_dht.DHT11(board.D4)
+
     while True:
         try:
             # Print the values to the serial port
@@ -27,15 +22,27 @@ def dht_sensor():
             print(error.args[0])
             time.sleep(2.0)
             continue
-        #except Exception as error:
-            #dhtDevice.exit()
-            #raise error
+        except KeyboardInterrupt:
+            print("\nScript terminated by User.")
+            dhtDevice.exit()
+        except Exception as error:
+            dhtDevice.exit()
+            raise error
 
         time.sleep(2.0)
 
 def water_sensor():
-    try:
-        while True:
+
+    # Water Sensor
+    # Initial the Device
+    ADC = Adafruit_ADS1x15.ADS1115(busnum= 1)
+    ADC_Channel = 3
+    Gain = 1
+    Min_ADC_Value = 0
+    Max_ADC_Value = 32767 
+
+    while True:
+        try:
             # Read ADC Value
             adc_value = ADC.read_adc(ADC_Channel, gain=Gain)
 
@@ -44,7 +51,24 @@ def water_sensor():
 
             print(f"ADC Value: {adc_value} | Water Level: {water_level:.2f}%")
 
-            time.sleep(1)
+        except KeyboardInterrupt:
+            print("\nScript terminated by User.")
 
-    except KeyboardInterrupt:
-        print("\nScript terminated by User.")
+        time.sleep(1)
+
+def ultrasonic_sensor():
+    # Ultrasonic Sensor for Water level
+    # Initial Device
+    sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D22, echo_pin=board.D27)
+
+    while True:
+        try:
+            print(sonar.distance, f"cm")
+        except RuntimeError:
+            print("Retrying!")
+        except KeyboardInterrupt:
+            print("\nScript terminated by User.")
+
+        time.sleep(1)
+
+ultrasonic_sensor()
